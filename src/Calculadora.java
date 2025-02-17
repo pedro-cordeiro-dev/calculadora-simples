279133edcd69e3859d9ed03b9b63005acd1246ac
pedroLucas <pedrolucassaraiva_@outlook.com>
2025-02-16 23:09:31 -0300

    Adicionadas as variáveis correspondentes e o método de cálculo para as operações matemáticas.

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -1,5 +1,32 @@
 public class Calculadora {
 
+    public String operacao;
+    public double num1;
+    public double num2;
 
+    public static double calcular(double num1, double num2 , String operacao) {
+
+        switch (operacao) {
+
+            case "+":
+                return num1 + num2;
+            case "-":
+                return num1 - num2;
+            case "*":
+                return num1 * num2;
+            case "/":
+                if(num1 != 0 && num2 !=0) {
+                    return num1 / num2;
+                }
+                else {
+                    throw new IllegalArgumentException("não é possivel dividir por 0");
+                }
+            case "%":
+                return num1 % num2;
+            default:
+                throw new IllegalArgumentException("operação desconhecida:" + operacao);
+        }
+
+    }
 
 }
